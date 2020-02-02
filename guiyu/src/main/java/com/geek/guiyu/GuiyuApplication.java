e1c69d1b7035efa423b79b2be10a7bc96a19e0dc
13370730939 <2848694359@qq.com>
2020-02-02 10:52:44 +0800

    平台搭建

---
+++ guiyu/src/main/java/com/geek/guiyu/GuiyuApplication.java
@@ -0,0 +1,13 @@
+package com.geek.guiyu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GuiyuApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GuiyuApplication.class, args);
+    }
+
+}
---
+++ guiyu/src/test/java/com/geek/guiyu/GuiyuApplicationTests.java
@@ -0,0 +1,13 @@
+package com.geek.guiyu;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GuiyuApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
